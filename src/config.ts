c2618114a8a2fe1696d8697f9f927f33d7ded312
zenith391 <39484230+zenith391@users.noreply.github.com>
2025-10-26 20:59:15 +0100

    fix: ROOT_NAME value in config.ts
    Without the additional dirname, the ROOT_NAME ends up being inside the
    'modules' folder, which caused the webui's initialisation to fail

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 // you *MUST* change this to the address of your server (otherwise some features like thumbnails won't work) !
 const Config = {
   HOST: "https://bwsecondary.ddns.net:8080",
-  ROOT_NAME: dirname(fileURLToPath(import.meta.url)),
+  ROOT_NAME: dirname(dirname(fileURLToPath(import.meta.url))),
   EARLY_ACCESS: true,
   VERSION: "0.9.3",
   /// How many worlds each player can have
